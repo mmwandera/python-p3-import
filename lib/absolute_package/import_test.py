bec84e03a9ef84412dc0a0b1e192d26927ee49fb
mmwandera <mmwandera74@gmail.com>
2024-01-20 13:37:57 +0300

    completed assignment

---
+++ lib/absolute_package/import_test.py
@@ -0,0 +1,15 @@
+# ABSOLUTE IMPORTS
+# An absolute import specifies the module to be imported using the full path from the project root directory.
+
+# from package1 import module1
+
+# module1.function1()
+
+from package1.module1 import function1
+
+function1()
+
+from package2.subpackage1.module6 import function1
+
+function1()
+
